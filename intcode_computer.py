2d275f977544d82d9ef5267197359ad275eeeb08
Sylvan <sylver@corp.delaval.com>
2019-12-06 01:35:55 +0100

    WIP

---
+++ intcode_computer.py
@@ -0,0 +1,120 @@
+class IntcodeComputer:
+    '''
+    Represents an Intcode Computer
+    '''
+
+    def __init__(self, program):
+        '''
+        Instance an Intcode Computer and load a program
+        '''
+        self._intCodeProgram = program
+        self._memoryPosition = 0
+
+    def addOP(self, paramode):
+        print("ADD")
+        print(paramode)
+        p1 = self._intCodeProgram[self._memoryPosition + 1]
+        p2 = self._intCodeProgram[self._memoryPosition + 2]
+        p3 = self._intCodeProgram[self._memoryPosition + 3]
+        if paramode[-1] == 0:
+            t1 = self._intCodeProgram[p1]
+        else: 
+            t1 = p1
+        
+        if paramode[-2] == 0:
+            t2 = self._intCodeProgram[p2]
+        else: 
+            t2 = p2
+        self._intCodeProgram[p3] = int(t1 + t2)
+        return 3 # OP size
+        
+
+    def multiplyOP(self, paramode):
+        print("MULTIPLY")
+        print(paramode)
+        p1 = self._intCodeProgram[self._memoryPosition + 1]
+        p2 = self._intCodeProgram[self._memoryPosition + 2]
+        p3 = self._intCodeProgram[self._memoryPosition + 3]
+        if paramode[-1] == 0:
+            f1 = self._intCodeProgram[p1]
+        else: 
+            f1 = p1
+        
+        if paramode[-2] == 0:
+            f2 = self._intCodeProgram[p2]
+        else: 
+            f2 = p2
+        self._intCodeProgram[p3] = int(f1 * f2)
+        return 3 # OP size
+
+    def inputOP(self, paramode):
+        p1 = self._intCodeProgram[self._memoryPosition + 1]
+        i1 = input("?")
+        # Todo: check, only number 0-9
+        self._intCodeProgram[p1] = int(i1)
+        return 1
+    
+    def outputOP(self, paramode):
+        p1 = self._intCodeProgram[self._memoryPosition + 1]
+        print(f'Output:{self._intCodeProgram[p1]}')
+        return 1
+
+
+    def getoperation(self, word):
+        opNo = int(word[-2:])
+
+        if opNo == 1:
+            size = 3
+            op = self.addOP
+        elif opNo == 2:
+            size = 3
+            op = self.multiplyOP
+        elif opNo == 3:
+            size = 1
+            op = self.inputOP
+        elif opNo == 4:
+            op = self.outputOP
+            size = 1
+        else:
+            assert False, f'Invalid operation code ({opNo}).'
+        return op, size
+
+    def getParameterMode(self, word, size):
+        l1 =  [0 for x in range(size)]
+        l2 =  [int(i) for i in word[:-2]]
+        if len(l2) > 0:
+            for x in range(-1,-size,-1):
+                l1[x] = l2[x]
+        return(l1)
+        
+
+    def perform_one_operation(self, memPos):
+        self._memoryPosition = memPos
+        halt = False
+        opword = str(self._intCodeProgram[self._memoryPosition])
+        op, opsize = self.getoperation(opword)
+        paramode = self.getParameterMode(opword, opsize)
+        print(f'mempos:{self._memoryPosition}\nword:{opword}\nOperation:{op}\npsize:{opsize}\nparamode:{paramode}\n')
+        op(paramode) # Perform opeartion
+        self._memoryPosition += opsize + 1
+
+
+
+
+
+if __name__ == "__main__":
+    IntCode = IntcodeComputer([1001,4,3,4,33])
+    IntCode.perform_one_operation(0)
+    print(IntCode._intCodeProgram)
+
+
+
+        # value1 = intCodeProgram[intCodeProgram[position+1]]
+        # value2 = intCodeProgram[intCodeProgram[position+2]]
+        # outputPosition = intCodeProgram[position+3]
+        # if opcode == 1:
+        #     intCodeProgram[outputPosition] = value1+value2
+        # elif opcode == 2:
+        #     intCodeProgram[outputPosition] = value1*value2
+        # position = position+4
+        # return nextMemPos, halt 
--- test/day_05_test.py
+++ test/day_05_test.py
@@ -1,10 +1,31 @@
 import unittest
 import day_05
+from intcode_computer import IntcodeComputer 
 
 class TestDay05day_05(unittest.TestCase):
     
-    def test_diagnostic_program(self):
-        self.assertEqual(day_05.runDiagnosticProgram([1002,4,3,4,33]), 0)
+
+
+
+    def test_multiply(self):
+        IC = IntcodeComputer([1002,4,3,4,33])
+        IC.perform_one_operation(0)
+        self.assertEqual(IC._intCodeProgram,[1002,4,3,4,12])
+
+
+    def test_add(self):
+        IC = IntcodeComputer([1002,2,2,4,33])
+        IC.perform_one_operation(0)
+        self.assertEqual(IC._intCodeProgram,[1002,2,2,4,4])
+
+    def test_input(self):
+        IC = IntcodeComputer([1003,4,2,4,33])
+        IC.perform_one_operation(0)
+        self.assertEqual(IC._intCodeProgram,[1002,2,2,4,4])
+
+# @unittest.SkipTest('Not implemeted yet')
+#     def test_diagnostic_program(self):
+#         self.assertEqual(day_05.runDiagnosticProgram([1002,4,3,4,33]), 0)
 
 
         
