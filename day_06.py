68648ae6687aa2c8c04e43c2fe36f6a899b45d7b
Sylvan <sylver@corp.delaval.com>
2019-12-07 00:06:56 +0100

    WIP

--- day_06.py
+++ day_06.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2019/day/6
 
 import copy
+from collections import defaultdict
 
 inputFile = 'input/06_input'
 
@@ -13,11 +14,35 @@ def getInputData():
             data.append(str(line).replace('\n','').replace('\r',''))
     return data
 
+def countOutersteps(orbitDictonary,satellite,step):
+    deep=step
+    for s in satellite:
+        step=deep
+        print(f'Visiting: {s} step:{step}')
+        result=step
+        print(f'Child: {orbitDictonary.get(s,"Not centre")}')
+        if orbitDictonary.get(s,"Not centre")=="Not centre":
+            print(step)
+        else:
+            step+=1
+            print(step)
+            nextSatellite=orbitDictonary.get(s)
+            result+=countOutersteps(orbitDictonary,nextSatellite,step)
+            print(step)
+            
+    return result
+
 def noOfOrbits(orbitMap):
-    '''
-    To be implemented
-    '''
-    return(len(orbitMap))
+    orbitDictonary = defaultdict(list)
+    for orbit in orbitMap:
+        centre=orbit.split(')')[0]
+        satellite=orbit.split(')')[1]
+        orbitDictonary[centre].append(satellite)
+    print(f'orbitDictonary:{orbitDictonary}')
+    satellite=orbitDictonary.get("COM")
+    nbrOrbits=countOutersteps(orbitDictonary,satellite,1) 
+    return nbrOrbits  
+
 
 
 def day06PartOne():
@@ -30,11 +55,3 @@ def day06PartOne():
 def day06PartTwo():
     answer = "unknown"
     print(f'Solution Day 06, Part two:\nAnswer: {answer} \n\n')
-
-
-if __name__ == "__main__":
-    day06PartOne()
-    day06PartTwo()
-
-# Run from terminal:
-# $ python day_06.py
