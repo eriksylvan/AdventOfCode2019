e1b622b62f6bd301611d99eb42a89cc15ecb54c3
Sylvan <sylver@corp.delaval.com>
2019-12-06 21:18:27 +0100

    day 5 part 2 branch

--- test/day_05_test.py
+++ test/day_05_test.py
@@ -10,18 +10,18 @@ class TestDay05(unittest.TestCase):
     def test_multiply(self):
         IC = IntcodeComputer([1002,4,3,4,33])
         IC.perform_one_operation(0)
-        self.assertEqual(IC._intCodeProgram,[1002,4,3,4,12])
+        self.assertEqual(IC._intCodeProgram,[1002,4,3,4,99])
 
 
     def test_add(self):
-        IC = IntcodeComputer([1002,2,2,4,33])
+        IC = IntcodeComputer([1001,2,2,4,33])
         IC.perform_one_operation(0)
-        self.assertEqual(IC._intCodeProgram,[1002,2,2,4,4])
+        self.assertEqual(IC._intCodeProgram,[1001,2,2,4,4])
 
-    def test_input(self):
-        IC = IntcodeComputer([1003,4,2,4,33])
-        IC.perform_one_operation(0)
-        self.assertEqual(IC._intCodeProgram,[1002,2,2,4,4])
+    # def test_input(self):
+    #     IC = IntcodeComputer([1003,4,2,4,33])
+    #     IC.perform_one_operation(0)
+    #     self.assertEqual(IC._intCodeProgram,[1002,2,2,4,4])
 
 # @unittest.SkipTest('Not implemeted yet')
 #     def test_diagnostic_program(self):
