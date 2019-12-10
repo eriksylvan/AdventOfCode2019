6cfef24b5e651ab8955780c5cb5060411ee2fab8
Sylvan <sylver@corp.delaval.com>
2019-12-11 00:08:59 +0100

    Solution day 10 part 1

--- day_10.py
+++ day_10.py
@@ -4,13 +4,20 @@ import copy
 
 inputFile = 'input/10_input'
 
+class Astroid:
+
+    def __init__(self,pos):
+        self._pos = pos
+        self._noVisibleFromMe = 0 #start fom 0
+    def __str__(self):
+        return str(self._pos)
 
 def getInputData():
     '''Reads the input file end returns the data in a list of int'''
     data = []
     with open(inputFile) as input:
         for line in input:
-            data = [int(x) for x in line.split(',')]
+            data.append(line.strip()) # strip() removes tailing \n
     return data
 
 def gcd(a, b):
@@ -27,12 +34,79 @@ def contVisibleAsroids(astroidMap):
     '''
     Not yet implemented
     '''
-    print(astroidMap)
+
     return (len(astroidMap) * len(astroidMap[0]), (0,0))
 
+
+
+def findBestMonitoringStation(map):
+    '''
+    The best location is the asteroid that can detect the largest number of other asteroids.
+    Returning #asdroids and Position
+    '''
+
+
+    # Get unique set of angle vectors 
+    hight = len(map)
+    width = len(map[0])
+    aList = []
+    for y in range(-hight, hight):
+        for x in range(-width, width):
+            aList.append((x,y))
+
+    naList = []
+    for a in aList:
+        d = abs(gcd(a[0],a[1]))
+       
+        if d!=0:
+            na = (a[0]//d, a[1]//d)
+        else:
+            na = (a[0], a[1])
+        naList.append(na)
+
+    uniqueAngleSet = set(naList)
+    uniqueAngleSet.discard((0,0))
+
+    # Get Astroid coodrinates (cartesian x,y)
+    astroids = {}
+    backup = {}
+    for yy, line in enumerate(map):
+        for xx, p in enumerate(line):         
+            if p == '#':
+                astroids[(xx,yy)] = Astroid((xx,yy))
+                backup[(xx,yy)] = 0
+           
+
+    # For ecery asdroid, find its closest visible neighbours and count them 
+    for astPos, astVal in astroids.items():
+        counter=0
+        for angle in uniqueAngleSet:
+            sx = astPos[0] + angle[0]
+            sy = astPos[1] + angle[1]
+            while (0 <= sx <= width) and (0 <= sy <= hight):
+                if (sx,sy) in astroids.keys():
+                    counter+=1
+                    break
+                sx += angle[0]
+                sy += angle[1]
+
+        astVal._noVisibleFromMe = counter
+        maxCount = 0
+        maxPos = (-1,-1) 
+
+    for ast in astroids.values():
+        if ast._noVisibleFromMe > maxCount:
+            maxCount = ast._noVisibleFromMe
+            maxPos = ast._pos    
+    
+    return maxCount, maxPos        
+
+
 def day10PartOne():
-    answer = "unknown"
-    print(f'Solution Day 10, Part one:\nAnswer: {answer} \n\n')
+    androidMap = getInputData()
+    count, pos = findBestMonitoringStation(androidMap)  
+    
+    print(f'Solution Day 10, Part one:\nBest Androis monitoring spot is: {pos}\nNumber of visible asdroids: {count} \n\n')
 
 
 def day10PartTwo():
@@ -41,8 +115,17 @@ def day10PartTwo():
 
 
 if __name__ == "__main__":
+    
     day10PartOne()
-    day10PartTwo()
+    # day10PartTwo()
 
 # Run from terminal:
 # $ python day_10.py
+
+
+
+
+
+# Solution Day 10, Part one:
+# Best Androis monitoring spot is: (23, 20)
+# Number of visible asdroids: 334
--- test/day_10_test.py
+++ test/day_10_test.py
@@ -4,19 +4,19 @@ import day_10
 class TestDay10(unittest.TestCase):
     
     def test_AstroidMap1(self):
-        self.assertEqual(day_10.contVisibleAsroids(AstridMap1),(8,(3,4)))
+        self.assertEqual(day_10.findBestMonitoringStation(AstridMap1),(8,(3,4)))
 
     def test_AstroidMap2(self):
-        self.assertEqual(day_10.contVisibleAsroids(AstridMap2),(33,(5,8)))
+        self.assertEqual(day_10.findBestMonitoringStation(AstridMap2),(33,(5,8)))
 
     def test_AstroidMap3(self):
-        self.assertEqual(day_10.contVisibleAsroids(AstridMap3),(35,(1,2)))
+        self.assertEqual(day_10.findBestMonitoringStation(AstridMap3),(35,(1,2)))
 
     def test_AstroidMap4(self):
-        self.assertEqual(day_10.contVisibleAsroids(AstridMap4),(41,(6,3)))
+        self.assertEqual(day_10.findBestMonitoringStation(AstridMap4),(41,(6,3)))
 
     def test_AstroidMap5(self):
-        self.assertEqual(day_10.contVisibleAsroids(AstridMap5),(210,(11,13)))
+        self.assertEqual(day_10.findBestMonitoringStation(AstridMap5),(210,(11,13)))
 
 
     @unittest.skip("Skip")
