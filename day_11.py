80508420a14d937ff842786a322bda696746608f
Sylvan <sylver@corp.delaval.com>
2019-12-12 12:21:33 +0100

    Solution day 11 part 2

--- day_11.py
+++ day_11.py
@@ -3,16 +3,20 @@
 import random
 import copy
 from intcode_computer import IntcodeComputer
+from PIL import Image, ImageDraw   # python -m pip install pillow
+
 
 
 inputFile = 'input/11_input'
 
 class SolarPanelMap:
-    def __init__(self):
+    def __init__(self, defaultColor=0):
+        self._defaultColor = defaultColor
         self._currentPositionX = 0   # Cartesian coordinates
         self._currentPositionY = 0
         self._currentDirection = 'U' # The robot starts facing up. (U, D, L, R)
-        self._panelMap = {(0,0):0} # {position(x,y):color 0=black 1=white}
+        self._panelMap = {(0,0):self._defaultColor} # {position(x,y):color 0=black 1=white}
+        
 
     def moveForward(self):
         if self._currentDirection == 'U':
@@ -26,7 +30,8 @@ class SolarPanelMap:
         else:
             assert False, 'Direction error'
         if not (self._currentPositionX, self._currentPositionY) in self._panelMap: 
-            self._panelMap[(self._currentPositionX, self._currentPositionY)] = 0 # add black 
+            self._panelMap[(self._currentPositionX, self._currentPositionY)] = self._defaultColor # add default color 
+            #print(self._panelMap[(self._currentPositionX, self._currentPositionY)])
 
     def turnLeft(self):
         if self._currentDirection == 'U':
@@ -67,6 +72,9 @@ class SolarPanelMap:
     def getColor(self):
         return self._panelMap.get((self._currentPositionX,self._currentPositionY))
 
+    def countPaintedPanels(self):
+        return len(self._panelMap)
+
 
 def getInputData():
     '''Reads the input file end returns the data in a list of int'''
@@ -83,27 +91,16 @@ def getAnwer(a):
     return -a
 
 
-def day11PartOne():
-    prg = getInputData()
-    print(prg)
-    
-    answer = "unknown"
-    print(f'Solution Day 11, Part one:\nAnswer: {answer} \n\n')
 
-
-def day11PartTwo():
-    answer = "unknown"
-    print(f'Solution Day 11, Part two:\nAnswer: {answer} \n\n')
-
-def startRobot():
+def startRobot(defaultColor=0):
     code = getInputData()
     IC = IntcodeComputer(code)
     
 
-    robotOnMap = SolarPanelMap()
+    robotOnMap = SolarPanelMap(defaultColor=defaultColor)
 
     step=0
-    inp=[0]
+    inp=[defaultColor]
     IC._output = []    # Clear the output list
     terminate = False
     stoppedAtInput = False
@@ -113,7 +110,7 @@ def startRobot():
         step +=1
         # print(f'### STEP {step} ###')
         # print(f'OutPut{IC._output}')
-        paintBlack = IC._output.pop(0) == 0
+        paintBlack = (IC._output.pop(0) == 0)
         # print(f'OutPut{IC._output}')        
 
         turnLeft = (IC._output.pop(0) == 0)
@@ -134,16 +131,43 @@ def startRobot():
         color = robotOnMap.getColor() # 0=black, 1=white
         inp = [color]
         stoppedAtInput = False
+    val = robotOnMap.countPaintedPanels()
+    return val, robotOnMap._panelMap
+
+def printMap(panel):
+
+    # PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
+    img = Image.new('1', (500, 500),color = 1)  # create a new black 1-bit image (BW)
+    pixels = img.load()         # create the pixel map
+    for pos in panel.items():
+        # print(pos)
+        x=pos[0][0]+200
+        y=pos[0][1]+200
+        color=pos[1]
+        pixels[x, y] = color
+    img.show()
+
     
-    print(f'Robot: {len(robotOnMap._panelMap)}')
+
+def day11PartOne():
+    answer, m = startRobot()
+    print(f'Solution Day 11, Part one:\nAnswer: {answer} \n\n')
+    printMap(m)
+
+def day11PartTwo():
+    answer, m = startRobot(defaultColor=1) # default color white
+    printMap(m)
+    
+
+    answer = "See picture"
+    print(f'Solution Day 11, Part two:\nAnswer: {answer} \n\n')
 
 
 
 if __name__ == "__main__":
-    startRobot()
-
-    # day11PartOne()
-    # day11PartTwo()
+    
+    day11PartOne()
+    day11PartTwo()
 
 # Run from terminal:
 # $ python day_11#.py
