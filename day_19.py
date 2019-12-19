8c23e6e2e6a8052cf91ce1fb53f34e1df8384fbb
Sylvan <sylver@corp.delaval.com>
2019-12-19 14:34:42 +0100

    Solution day 19 part 1

--- day_19.py
+++ day_19.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2019/day/19
 
 import copy
+from intcode_computer import IntcodeComputer
 
 inputFile = 'input/19_input'
 
@@ -13,12 +14,36 @@ def getInputData():
             data = [int(x) for x in line.split(',')]
     return data
 
+def drone():
+    code = getInputData()
+    # IC = IntcodeComputer(code)
+#    out = {}
+    pic = []
+    count = 0
+    for y in range(50):
+        row = ''
+        for x in range(50):
+            IC = IntcodeComputer(code)
+            o = IC.run_program([x,y])
+            if o[0] == 1:
+                count +=1
+                row += '#'
+            else:
+                row += '.'
+            
+        pic.append(row)
+
+    # print(out)
+    for r in pic: print(r)
+    
+    return count
+    
 def getAnwer(v):
     return -v*v
 
 def day19PartOne():
-    answer = "unknown"
-    print(f'Solution Day 19, Part one:\nAnswer: {answer} \n\n')
+    answer = drone()
+    print(f'Solution Day 19, Part one:\nTractor beam affects: {answer} positions within 50x50 range \n\n')
 
 
 def day19PartTwo():
@@ -27,6 +52,7 @@ def day19PartTwo():
 
 
 if __name__ == "__main__":
+    # drone()
     day19PartOne()
     day19PartTwo()
 
