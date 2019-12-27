66a8e7d02796e674c52ad7467511a9556d8dcb23
Erik Sylvan <erik.sylvan@gmail.com>
2019-12-27 02:49:09 +0100

    Meery xmas

---
+++ merry_xmas.py
@@ -0,0 +1,26 @@
+import copy
+from intcode_computer import IntcodeComputer
+
+inputFile = 'input/merry_xmas'
+
+
+def getInputData():
+    '''Reads the input file end returns the data in a list of int'''
+    data = []
+    with open(inputFile) as input:
+        for line in input:
+            data = [int(x) for x in line.split(',')]
+    return data
+    
+    
+    
+if __name__ == "__main__":
+	prg = getInputData()
+	#print(prg)
+	IC = IntcodeComputer(prg)
+	IC.run_program()
+	str = ""
+	str = str.join(([chr(ch) for ch in IC._output]))
+	print(str)
+   
+   
